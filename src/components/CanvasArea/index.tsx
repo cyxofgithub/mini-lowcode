406afd3ef5be69537ef8ea48a907d47cecfe099d
chenyuanxin <chenyuanxin@cvte.com>
2023-12-29 15:10:41 +0800

    feat: 抽离渲染区域逻辑

---
+++ src/components/CanvasArea/index.tsx
@@ -0,0 +1,35 @@
+import './index.less';
+
+//私有常量
+
+//可抽离的逻辑处理函数/组件
+
+/**
+ * 【组件功能】
+ *
+ * 【应用模块】
+ *
+ */
+
+let CanvasArea = (_props: IProps) => {
+    //变量声明、解构
+
+    //组件状态
+
+    //网络IO
+
+    //数据转换
+
+    //逻辑处理函数
+
+    //组件Effect
+
+    return <div id="canvas-container">编辑区 - 画布</div>;
+};
+
+//props类型定义
+interface IProps {}
+
+//prop-type定义，可选
+
+export { CanvasArea };

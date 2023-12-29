5d5fcc411febaa8a65b72e2a4eac249606c2c362
chenyuanxin <chenyuanxin@cvte.com>
2023-12-29 15:11:55 +0800

    faet: 增加测试schema

---
+++ src/schema/index.ts
@@ -0,0 +1,11 @@
+const schema = {
+    container: {
+        width: 375, // iPhone 6/7/8 尺寸（具体按照设计稿尺寸）
+        height: 667,
+    },
+    blocks: [
+        // 画布中拖拽的元素集合，下面介绍
+    ],
+};
+
+export default schema;

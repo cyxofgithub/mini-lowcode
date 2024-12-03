6c56465fd5e58aed19aa747c5025f97067f04195
chenyuanxin <chenyuanxin@cvte.com>
2024-12-03 19:11:12 +0800

    feat: 完善容器辅助线逻辑

--- src/components/CanvasArea/utils/index.ts
+++ src/components/CanvasArea/utils/index.ts
@@ -1,3 +1,4 @@
+import { globalStore } from '../../../store';
 import { IAuxiliaryLines } from '../declare';
 import { IBlock } from '../declare/block';
 
@@ -32,40 +33,48 @@ export const getLinesPlaceInfo = (focusBlock: IBlock, unFocusBlocks: IBlock[]) =
                 ? unFocusedBlockLeft - focusBlock.style.left + unFocusedBlockWidth
                 : focusBlock.style.left - unFocusedBlockLeft + focusBlockWidth;
 
+        const container = globalStore.currentSchema.container;
+        const isFilledContainerBlock =
+            container.width === unFocusedBlockWidth && container.height === unFocusedBlockHeight;
+
+        // 容器也是一个 block，用于实现对其容器边界时的辅助线
+        const xLength = isFilledContainerBlock ? container.height : xLineLength;
+        const yLength = isFilledContainerBlock ? container.width : yLineLength;
+
         // 垂直纵线显示的 5 种情况：（A代表未聚焦元素， B代表聚焦元素）
         // 情况一：左对左，线条展示在 A 的左边
         lines.x.push({
             lineLeft: unFocusedBlockLeft,
             lineTop: xLineTop,
-            length: xLineLength,
+            length: xLength,
             triggerLeft: unFocusedBlockLeft,
         });
         // 情况二：左对右，线条显示在 A 的左边
         lines.x.push({
             lineLeft: unFocusedBlockLeft,
             lineTop: xLineTop,
-            length: xLineLength,
+            length: xLength,
             triggerLeft: unFocusedBlockLeft - focusBlockWidth,
         });
         // 情况三：右对左，线条显示在 A 的右边
         lines.x.push({
             lineLeft: unFocusedBlockLeft + unFocusedBlockWidth,
             lineTop: xLineTop,
-            length: xLineLength,
+            length: xLength,
             triggerLeft: unFocusedBlockLeft + unFocusedBlockWidth,
         });
         // 情况四：右对右，线条显示在 A 的右边
         lines.x.push({
             lineLeft: unFocusedBlockLeft + unFocusedBlockWidth,
             lineTop: xLineTop,
-            length: xLineLength,
+            length: xLength,
             triggerLeft: unFocusedBlockLeft + unFocusedBlockWidth - focusBlockWidth,
         });
         // 情况五：中对中，线条显示在 A 的中间
         lines.x.push({
             lineLeft: unFocusedBlockLeft + unFocusedBlockWidth / 2,
             lineTop: xLineTop,
-            length: xLineLength,
+            length: xLength,
             triggerLeft: unFocusedBlockLeft + unFocusedBlockWidth / 2 - focusBlockWidth / 2,
         });
 
@@ -74,35 +83,35 @@ export const getLinesPlaceInfo = (focusBlock: IBlock, unFocusBlocks: IBlock[]) =
         lines.y.push({
             lineLeft: yLineLeft,
             lineTop: unFocusedBlockTop,
-            length: yLineLength,
+            length: yLength,
             triggerTop: unFocusedBlockTop,
         });
         // 情况二：A 和 B 顶对底，线条显示在 A 的上边
         lines.y.push({
             lineLeft: yLineLeft,
             lineTop: unFocusedBlockTop,
-            length: yLineLength,
+            length: yLength,
             triggerTop: unFocusedBlockTop - focusBlockHeight,
         });
         // 情况三：A 和 B 底对顶， 线条显示在 A 的下边
         lines.y.push({
             lineLeft: yLineLeft,
             lineTop: unFocusedBlockTop + unFocusedBlockHeight,
-            length: yLineLength,
+            length: yLength,
             triggerTop: unFocusedBlockTop + unFocusedBlockHeight,
         });
         // 情况四：A 和 B 底对底， 线条显示在 A 的下边
         lines.y.push({
             lineLeft: yLineLeft,
             lineTop: unFocusedBlockTop + unFocusedBlockHeight,
-            length: yLineLength,
+            length: yLength,
             triggerTop: unFocusedBlockTop + unFocusedBlockHeight - focusBlockHeight,
         });
         // 情况五：A 和 B 中对中， 线条显示在 A 的中间
         lines.y.push({
             lineLeft: yLineLeft,
             lineTop: unFocusedBlockTop + unFocusedBlockHeight / 2,
-            length: yLineLength,
+            length: yLength,
             triggerTop: unFocusedBlockTop + unFocusedBlockHeight / 2 - focusBlockHeight / 2,
         });
     });
